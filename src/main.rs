8aa41829295f2b8755c786c1a02e6fb4c82fe6dc
Kevin Pyc <dev.kpyc@gmail.com>
2017-01-02 17:57:38 -0500

    Pick one random hint at a time.

--- src/main.rs
+++ src/main.rs
@@ -11,22 +11,25 @@ fn main() {
     let mut fruit_properties = MultiMap::new();
     fruit_properties.insert("Apple", "Red");
     fruit_properties.insert("Apple", "Round");
+    fruit_properties.insert("Apple", "Sweet");
     fruit_properties.insert("Orange", "Round");
     fruit_properties.insert("Orange", "Orange");
 
-    let random_fruit = rand::thread_rng().choose(&fruits).unwrap();
-    match fruit_properties.get_vec(random_fruit) {
-        Some(properties) => {
-            for property in properties {
-                println!("It's {}", property);
-            }
-        },
-        None => println!("I can't describe this item.")
+    let mut rng = rand::thread_rng();
+
+    let random_fruit = rng.choose(&fruits).unwrap();
+    let mut remaining_hints;
+    if let Some(properties) = fruit_properties.get_vec(random_fruit) {
+        remaining_hints = properties.clone();
+        rng.shuffle(&mut remaining_hints);
+    } else {
+        remaining_hints = Vec::new();
     }
-    println!("What am I thinking of?");
+
+    println!("What fruit am I thinking of?");
+    println!("Type \"Hint\" for a hint!");
     
     loop {
-        
         let mut guess = String::new();
         io::stdin().read_line(&mut guess)
             .expect("Failed to read line");
@@ -34,11 +37,16 @@ fn main() {
         if &guess.trim() == random_fruit {
             println!("You guessed it!");
             break;
-        }
-        else {
+        } else if guess.trim() == "Hint" {
+            if let Some(hint) = remaining_hints.pop() {
+                println!("It's {}", hint);
+            }
+            else {
+                println!("I have no more hints to give you!");
+            }
+        } else {
             println!("Nope! Guess again.");
         }
     }
-
     //println!("I picked {}", random_fruit);
 }
